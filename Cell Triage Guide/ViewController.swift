ace59954565b882b324ae4d197cdd46835123e1f
Jason Kwong <saikitJK@macbook-pro.local>
2018-02-07 16:31:48 -0800

    Initial Commit

---
+++ Cell Triage Guide/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cell Triage Guide
+//
+//  Created by saikitJK on 2/7/18.
+//  Copyright © 2018 Tacoma Police Department. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Cell Triage GuideTests/Cell_Triage_GuideTests.swift
@@ -0,0 +1,36 @@
+//
+//  Cell_Triage_GuideTests.swift
+//  Cell Triage GuideTests
+//
+//  Created by saikitJK on 2/7/18.
+//  Copyright © 2018 Tacoma Police Department. All rights reserved.
+//
+
+import XCTest
+@testable import Cell_Triage_Guide
+
+class Cell_Triage_GuideTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
